e25bb9c356952015669e6bd967b0dc12c0a2953e
Gunnari Auvinen <gauvinen@gmail.com>
2014-12-03 22:29:08 -0500

    Add Rooms factory. Add addRoom function to Rooms factory.

---
+++ client/app/services/services.js
@@ -0,0 +1,22 @@
+angular.module('BS.services', ['firebase'])
+
+.factory('Rooms', function($firebase){
+  var addRoom = function(roomInfo) {
+    var ref = new Firebase('https://resplendent-inferno-1306.firebaseio.com/rooms');
+    var sync = $firebase(ref);
+    var rooms = sync.$asArray();
+
+    return rooms.$add(roomInfo)
+    .then(function(newChildRef) {
+      return newChildRef.key();
+    })
+    .catch(function(data) {
+      console.log('IN THE CATCH')
+      console.error(data)
+    })
+  };
+
+  return {
+    addRoom: addRoom
+  };
+})
